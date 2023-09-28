ce222c8c8452a85384da910d228f732c9d0a1720
pallak1 <128583368+pallak1@users.noreply.github.com>
2023-09-28 10:01:05 +0200

    Delete 3-4-óra directory

--- 3-4-óra/hazi_1.1.py
+++
@@ -1,7 +0,0 @@
-Nap=input("Milyen napod van? ")
-if Nap=="jó":
-    print("Az jó")
-elif Nap=="rossz":
-    print("Az nem jó")
-
-        
--- 3-4-óra/hazi_1.2.py
+++
@@ -1,9 +0,0 @@
-Nap=input("Milyen napod van? ")
-if Nap=="jó":
-    print("Az jó")
-elif Nap=="rossz":
-    print("Az nem jó")
-else:
-    print("Sajnos nem értem a válaszodat!")
-
-        
